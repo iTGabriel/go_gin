7e910136e27d7ce5e23370049f9a053a203a3b6a
Gabriel <gabriel.gustavo.s@hotmail.com>
2022-02-09 00:15:54 -0300

    Init gogin

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{"title": "Teste", "desc": "Gin - Goglang framework"})
+	})
+	router.Run(":8080")
+}
